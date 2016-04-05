ed050e97a9f7d4ad44535925060705f8928f8014
ayoola-solomon <ayoola.solomon89@gmail.com>
2016-04-05 11:34:34 +0100

    edited

--- config.js
+++ config.js
@@ -3,7 +3,7 @@ var config = {};
 config.db = {};
 config.webhost = 'http://localhost:3000/';
 
-config.db.host = 'localhost';
+config.db.host = 'localhost' || '52.58.102.26'; // mongodb aws instance public ip
 config.db.name = 'url_shortener';
 
 module.exports = config;
